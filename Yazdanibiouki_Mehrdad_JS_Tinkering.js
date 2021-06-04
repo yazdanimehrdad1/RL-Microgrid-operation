1bd0b4cb56a3c7e5f4c5a8d31de9f4a6b8e2d6ca
Mehrdad Yazdanibiouki <yazdani.mehrdad1@gmail.com>
2021-06-04 12:11:53 -0700

    Add files via upload

---
+++ Yazdanibiouki_Mehrdad_JS_Tinkering.js
@@ -0,0 +1,12 @@
+var childHeight = 20;
+
+
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight){
+
+    if (childHeight > 52){
+        console.log("Get on that ride, kiddo!");
+    }
+    else{
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
